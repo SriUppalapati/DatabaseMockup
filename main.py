1f81612d141fd69898c2144610e8d396f2174658
Sridhar <sridharuppalapati2@gmail.com>
2019-07-02 14:45:50 -0400

    Working with how to draw data out of the MongoDB database

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 #Main backend code#
+from tkinter import *
+import pymongo
+
+class Order(object):
+    def __init__(self, orderNum, company, cost, numParts, date):
+        self.orderNum = orderNum
+        self.company = company
+        self.cost = cost
+        self.numParts = numParts
+        self.date = date
+    def get_orderNum(self):
+        return self.orderNum
+    def get_cost(self):
+        return self.cost
+    def get_company(self):
+        return self.company
+    def get_numParts(self):
+        return self.numParts
+    def get_date(self):
+        return self.date
+    
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+mydict = {"_id": 8,"name": "John", "address":"Highway 37"}
+
+x = mycol.insert_one(mydict)
+
+for x in mycol.find({}, {"_id":0, "name": 1}):
+    print(x, type(x))
+    print(x['name'])
+
+
+print(myclient.list_database_names())
+dblist = myclient.list_database_names()
+if "mydatabase" in dblist:
+    print("The database exits.")
