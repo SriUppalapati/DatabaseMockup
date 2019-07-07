aa95cebc05bce34b03257d191b875003bb59c76b
Sridhar <sridharuppalapati2@gmail.com>
2019-07-07 11:07:31 -0400

    Still figuring out how to get data from entry blocks

--- OrderClasses.py
+++ OrderClasses.py
@@ -1,4 +1,5 @@
 #Classes for ordering and parts in order to make it easy to display in GUI
+import datetime
 class Order(object):
     def __init__(self, orderNum, company, cost, numParts, date):
         self.orderNum = orderNum
@@ -16,4 +17,28 @@ class Order(object):
         return self.numParts
     def get_date(self):
         return self.date
-    
+    def get_dict(self):
+        date = datetime.datetime.today()
+        return {"Order Number" : self.orderNum, 
+                "Cost" : self.cost, "Company" : self.company, 
+                "Number of Parts" : self.numParts,
+                "Date" : date.strftime('%d-%m-%Y')}
+
+class Part(object):
+    def __init__(self, orderNum, partNum, quote, notes):
+        self.orderNum = orderNum
+        self.partNum = partNum
+        self.quote = quote
+        self.notes = notes
+    def get_orderNum(self):
+        return self.orderNum
+    def get_partNum(self):
+        return self.partNum
+    def get_quote(self):
+        return self.quote
+    def get_notes(self):
+        return self.notes
+    def get_dict(self):
+        return {"Order Number" : self.orderNum, 
+                "Part Number" : self.partNum, 
+                "Quote" : self.quote, "Notes" : self.notes}
--- main.py
+++ main.py
@@ -3,23 +3,29 @@ import tkinter as tk
 import pymongo
 import OrderClasses
 
-#Function to make a new part and store in database
-def newPart():
+#Function to add new part to database and move file
+def newPart(orderNum, partNum, quote, notes):
+    print(orderNum, partNum, quote, notes)
+#Function to make a gui to add a new part
+def newPartGui():
     partGui = tk.Tk()
     tk.Label(partGui, text="Order Number").grid(row=0)
     tk.Label(partGui, text="Part Number").grid(row=1)
     tk.Label(partGui, text="Quote").grid(row=2)
     tk.Label(partGui, text="Notes").grid(row=3)
     
-    eON = tk.Entry(partGui).grid(row=0, column=1)
+    eON = tk.Entry(partGui)
+    eON.grid(row=0, column=1)
     ePN = tk.Entry(partGui).grid(row=1, column=1)
     eQ = tk.Entry(partGui).grid(row=2, column=1)
     eN = tk.Entry(partGui).grid(row=3, column=1)
-    
-    tk.Button(partGui, text='Quit', command=partGui.quit).grid(row=4,column=0, sticky=tk.W, pady=4)
-    
+    tk.Button(partGui, text='Quit', 
+              command=partGui.quit).grid(row=4,column=0, sticky=tk.W, pady=4)
+    tk.Button(partGui, text='Store', 
+              command=print(type(eON.get()))).grid(row=4,column=1, sticky=tk.W, pady=4)
     partGui.mainloop()
-        
+    
+    #newPart(eON.get, ePN.get, eQ.get, eN.get)    
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["MachineCompany"]
 orders = mydb["Orders"]
@@ -36,7 +42,7 @@ entryCN.grid(row=0,column=1)
 entryON.grid(row=1,column=1)
 
 tk.Button(master, text='Quit', command=master.quit).grid(row=4,column=0, sticky=tk.W, pady=4)
-tk.Button(master, text='New Part', command=newPart).grid(row=4,column=1, sticky=tk.W, pady=4)
+tk.Button(master, text='New Part', command=newPartGui).grid(row=4,column=1, sticky=tk.W, pady=4)
 
 master.mainloop()
 
